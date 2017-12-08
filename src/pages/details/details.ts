591275bbe2353f07f51234bd56845e8c1ac931d5
James Coyne <james.coyne@gmail.com>
2017-12-08 17:34:00 -0500

    Finish filling all fields with data from API. May need to fix right aligned values.

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -28,7 +28,7 @@ export class DetailsPage {
 
     this.systemID = this.navParams.get('systemID');
     if (this.systemID == null) {
-      this.systemID = 1;
+      this.systemID = 171;
       console.log('No SystemID passed, using default test value 1!');
     }
 
@@ -100,6 +100,7 @@ export class DetailsPage {
   		alert('No data!');
   		return;
   	}
+  	console.log(data);
  		this.items[0] += data['companyName'];
  		this.items[1] += data['systemName'];
  		this.items[2] += data['serialNumber'];
@@ -107,31 +108,123 @@ export class DetailsPage {
  		this.items[4] += data['model'];
  		this.items[5] += data['osVersion'];
  		this.items[6] += data['cpgCount'];
- 		(data['Recommended.osVersion'] == null) 
+ 		(data['Recommended_osVersion'] == null) 
  			? this.items[7] += 'N/A' 
- 			: this.items[7] += data['Recommended.osVersion'];
- 		(data[location.region] == null)
+ 			: this.items[7] += data['Recommended_osVersion'];
+ 		(data['location_region'] == 0)
  			? this.items[8] += 'N/A'
- 			: this.items[8] += data['location.region'];
- 		(data[location.country] == null)
+ 			: this.items[8] += data['location_region'];
+ 		(data['location_country'] == 0)
  			? this.items[9] += 'N/A'
- 			: this.items[9] += data['location.country'];
+ 			: this.items[9] += data['location_country'];
  		this.items[10] += data['installDate'];
- 		this.items[11] += data['updated'];
- 		(data['nodes.nodeCount'] == null)
+ 		(data['updated'] == 0)
+ 			? this.items[11] += 'N/A'
+ 			: this.items[11] += data['updated'];
+ 		(data['nodes_nodeCount'] == 0)
  			? this.items[12] += 'N/A'
- 			: this.items[12] += data['nodes.nodeCount'];
- 		(data['nodes.nodeCountOffline'] == null)
+ 			: this.items[12] += data['nodes_nodeCount'];
+ 		(data['nodes_nodeCountOffline'] == 0)
  			? this.items[13] += 'N/A'
- 			: this.items[13] += data['nodes.nodeCountOffline'];
- 		this.items[14] += data['disks.total.diskCount'];
- 		this.items[15] += data['disks.total.diskCountNormal'];
- 		this.items[16] += data['disks.total.diskCountDegraded'];
- 		this.items[17] += data['disks.total.diskCountFailed'];
+ 			: this.items[13] += data['nodes_nodeCountOffline'];
+ 			(data['disk_total_diskCount'] == 0)
+ 			? this.items[14] += 'N/A'
+ 			: this.items[14] += data['disks_total_diskCount'];
+ 		(data['disks_total_diskCountNormal'] == 0)
+ 			? this.items[15] += 'N/A'
+ 			: this.items[15] += data['disks_total_diskCountNormal'];
+ 		(data['disks_total_diskCountDegraded'] == 0)
+ 			? this.items[16] += 'N/A'
+ 			: this.items[16] += data['disks_total_diskCountDegraded'];
+ 		(data['disks_total_diskCountFailed'] == 0)
+ 			? this.items[17] += 'N/A'
+ 			: this.items[17] += data['disks_total_diskCountFailed'];
  		this.items[18] += data['disksState'];
  		this.items[19] += data['vvCount'];
- 		this.items[20] += data['tpvvCount'];
- 		this.items[21] += data['capacity.total.freePct'] + "%";
- 		this.items[22] += data['capacity.total.freeTiB'] + "TiB";
+ 		(data['tpvvCount'] == 0)
+ 			? this.items[20] += 'N/A'
+ 			: this.items[20] += data['tpvvCount'];
+ 		(data['capacity_total_freePct'] == 0)
+ 			? this.items[21] += 'N/A'
+ 			: this.items[21] += data['capacity_total_freePct'].toFixed(2) + '%';
+ 		(data['capacity_total_freeTiB'] == 0)
+ 			? this.items[22] += 'N/A'
+ 			: this.items[22] += data['capacity_total_freeTiB'].toFixed(2) + ' TiB';
+ 		(data['capacity_byType_fc_freeTiB'] == 0)
+ 			? this.items[23] += 'N/A'
+ 			: this.items[23] += data['capacity_byType_fc_freeTiB'].toFixed(2) + ' TiB';
+ 		(data['capacity_byType_nl_freeTiB'] == 0)
+ 			? this.items[24] += 'N/A'
+ 			: this.items[24] += data['capacity_byType_nl_freeTiB'].toFixed(2) + ' TiB';
+ 		(data['capacity_byType_ssd_freeTiB'] == 0)
+ 			? this.items[25] += 'N/A'
+ 			: this.items[25] += data['capacity_byType_ssd_freeTiB'].toFixed(2) + ' TiB';
+ 		(data['capacity_total_sizeTiB'] == 0)
+ 			? this.items[26] += 'N/A'
+ 			: this.items[26] += data['capacity_total_sizeTiB'].toFixed(2) + ' TiB';
+ 		(data['capacity_byType_fc_sizeTiB'] == 0)
+ 			? this.items[27] += 'N/A'
+ 			: this.items[27] += data['capacity_byType_fc_sizeTiB'].toFixed(2) + ' TiB';
+ 		(data['capacity_byType_nl_sizeTiB'] == 0)
+ 			? this.items[28] += 'N/A'
+ 			: this.items[28] += data['capacity_byType_nl_sizeTiB'].toFixed(2) + ' TiB';
+ 		(data['capacity_byType_ssd_sizeTiB'] == 0)
+ 			? this.items[29] += 'N/A'
+ 			: this.items[29] += data['capacity_byType_ssd_sizeTiB'].toFixed(2) + ' TiB';
+ 		(data['capacity_total_compactionRatio'] == 0)
+ 			? this.items[30] += 'N/A'
+ 			: this.items[30] += data['capacity_total_compactionRatio'].toFixed(2);
+ 		(data['capacity_total_compressionRatio'] == 0)
+ 			? this.items[31] += 'N/A'
+ 			: this.items[31] += data['capacity_total_compressionRatio'].toFixed(2);
+ 		(data['capacity_total_dedupeRatio'] == 0)
+ 			? this.items[32] += 'N/A'
+ 			: this.items[32] += data['capacity_total_dedupeRatio'].toFixed(2);
+ 		(data['performance_portBandwidthData_total_dataRateKBPSAvg'] == 0)
+ 			? this.items[33] += 'N/A'
+ 			: this.items[33] += data['performance_portBandwidthData_total_dataRateKBPSAvg'].toFixed(2)
+ 					+ ' KB/s';
+ 		(data['performance_portBandwidthData_total_iopsAvg'] == 0)
+ 			? this.items[34] += 'N/A'
+ 			: this.items[34] += data['performance_portBandwidthData_total_iopsAvg'].toFixed(2) 
+ 					+ ' IOPS';
+ 		(data['performance_portBandwidthData_total_iopsMax'] == 0)
+ 			? this.items[35] += 'N/A'
+ 			: this.items[35] += data['performance_portBandwidthData_total_iopsMax'].toFixed(2)
+ 					+ ' IOPS';
+ 		(data['performance_summary_portInfo_totalServiceTimeMillis'] == 0)
+ 			? this.items[36] += 'N/A'
+ 			: this.items[36] += data['performance_summary_portInfo_totalServiceTimeMillis'].toFixed(2)
+ 					+ ' ms';
+ 		(data['performance_summary_portInfo_readServiceTimeMillis'] == 0)
+ 			? this.items[37] += 'N/A'
+ 			: this.items[37] += data['performance_summary_portInfo_readServiceTimeMillis'].toFixed(2)
+ 					+ ' ms';
+ 		(data['performance_summary_portInfo_writeServiceTimeMillis'] == 0)
+ 			? this.items[38] += 'N/A'
+ 			: this.items[38] += data['performance_summary_portInfo_writeServiceTimeMillis'].toFixed(2)
+ 					+ ' ms';
+ 		(data['performance_summary_delAckPct'] == 0)
+ 			? this.items[39] += 'N/A'
+ 			: this.items[39] += data['performance_summary_delAckPct'].toFixed(2) + '%';
+ 		(data['performance_summary_vvInfo_vvsByType_ssd_readBandwidthMBPS'] == 0)
+ 			? this.items[40] += 'N/A'
+ 			: this.items[40] += data['performance_summary_vvInfo_vvsByType_ssd_readBandwidthMBPS'].toFixed(2)
+ 					+ ' MB/s';
+ 		(data['performance_summary_vvInfo_vvsByType_ssd_writeBandwidthMBPS'] == 0)
+ 			? this.items[41] += 'N/A'
+ 			: this.items[41] += data['performance_summary_vvInfo_vvsByType_ssd_writeBandwidthMBPS'].toFixed(2)
+ 					+ ' MB/s';
+ 		(data['performance_summary_vvInfo_vvsByType_ssd_readServiceTimeMillis'] == 0)
+ 			? this.items[42] += 'N/A'
+ 			: this.items[42] += data['performance_summary_vvInfo_vvsByType_ssd_readServiceTimeMillis'].toFixed(2)
+ 					+ ' ms';
+ 		(data['performance_summary_vvInfo_vvsByType_ssd_writeServiceTimeMillis'] == 0)
+ 			? this.items[43] += 'N/A'
+ 			: this.items[43] += data['performance_summary_vvInfo_vvsByType_ssd_writeServiceTimeMillis'].toFixed(2)
+ 					+ ' ms';
+ 		(data['nodes_cpuAvgMax'] == 0)
+ 			? this.items[44] += 'N/A'
+ 			: this.items[44] += data['nodes_cpuAvgMax'] + '%';
   }
 }
