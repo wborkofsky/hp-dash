d86c907fadcf5ad95bb9d3eb81ec5c4365193cae
Tanaya Asnani <tasnani@umass.edugit config --global user.name Tanaya Asnani>
2017-12-12 13:49:00 -0500

    Latest working dashboard with loaded data.

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {Chart} from 'chart.js'
+import {Chart} from 'chart.js';
+import {Api} from '../../app/api';
 
 @Component({
   selector: 'page-dashboard',
@@ -9,8 +10,8 @@ import {Chart} from 'chart.js'
 export class dashboardPage {
     @ViewChild('lineCanvasOne') lineCanvasOne;
     @ViewChild('doughnutCanvasOne') doughnutCanvasOne
-	@ViewChild('barCanvas') barCanvas;
-	@ViewChild('doughnutCanvasTwo') doughnutCanvasTwo;
+    @ViewChild('barCanvas') barCanvas;
+    @ViewChild('doughnutCanvasTwo') doughnutCanvasTwo;
     @ViewChild('lineCanvasTwo') lineCanvasTwo;
  
     lineChartOne: any;
@@ -19,18 +20,40 @@ export class dashboardPage {
     doughnutChartTwo: any;
     lineChartTwo: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public api:Api) {
+   
 
   }
+
 ionViewDidLoad() {
-        this.lineChartOne = new Chart(this.lineCanvasOne.nativeElement, {
+        var json_objs= [];
+        var cpg_count = [];
+        var disk_count =[];
+        var capacity_size = [];
+        var bandwidth =[];
+        var capacity_free =[];
+
+        var global_labels =["System1","System2","System3","System4","System5","System6","System7","System8","System9","System10"];
+        console.log(this)
+        this.api.listSystems( (dataArg) => {
+            //console.log(dataArg);
+            var i = 0;
+            for (i=0; i<dataArg.length; i++){
+            json_objs.push(dataArg[i]);
+            cpg_count.push(parseInt(dataArg[i].cpgCount));
+            disk_count.push(parseInt(dataArg[i].disks_total_diskCount));
+            capacity_size.push(parseInt(dataArg[i].capacity_total_sizeTiB));
+            capacity_free.push(parseInt(dataArg[i].capacity_total_freeTiB));
+            bandwidth.push(dataArg[i].performance_portBandwidthData_total_dataRateKBPSAvg);
+            console.log(this)
+            }
+            this.lineChartOne = new Chart(this.lineCanvasOne.nativeElement, {
  
             type: 'line',
             data: {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                labels: global_labels ,
                 datasets: [
                     {
-                        label: "My First dataset",
                         fill: false,
                         lineTension: 0.1,
                         backgroundColor: "rgba(75,192,192,0.4)",
@@ -48,22 +71,23 @@ ionViewDidLoad() {
                         pointHoverBorderWidth: 2,
                         pointRadius: 1,
                         pointHitRadius: 10,
-                        data: [65, 59, 80, 81, 56, 55, 40],
-                        spanGaps: false,
+                        data: cpg_count,
+                        spanGaps: true,
                     }
                 ]
             }
  
         });
+        this.lineChartOne.update();
 
         this.doughnutChartOne = new Chart(this.doughnutCanvasOne.nativeElement, {
  
             type: 'doughnut',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: global_labels,
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    label: 'disk_count',
+                    data: disk_count,
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -84,15 +108,15 @@ ionViewDidLoad() {
             }
  
         });
-
+        this.doughnutChartOne.update();
         this.barChart = new Chart(this.barCanvas.nativeElement, {
  
             type: 'bar',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: global_labels,
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    label: 'capacity_size',
+                    data: capacity_size,
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -123,15 +147,16 @@ ionViewDidLoad() {
             }
  
         });
+        this.barChart.update();
 
         this.doughnutChartTwo = new Chart(this.doughnutCanvasTwo.nativeElement, {
  
             type: 'doughnut',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: global_labels,
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    label: 'bandwidth',
+                    data: bandwidth,
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -152,15 +177,15 @@ ionViewDidLoad() {
             }
  
         });
-
+        this.doughnutChartTwo.update();
         this.lineChartTwo = new Chart(this.lineCanvasTwo.nativeElement, {
  
             type: 'line',
             data: {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                labels: global_labels,
                 datasets: [
                     {
-                        label: "My First dataset",
+                        label:'capacity free',
                         fill: false,
                         lineTension: 0.1,
                         backgroundColor: "rgba(75,192,192,0.4)",
@@ -178,12 +203,17 @@ ionViewDidLoad() {
                         pointHoverBorderWidth: 2,
                         pointRadius: 1,
                         pointHitRadius: 10,
-                        data: [65, 59, 80, 81, 56, 55, 40],
+                        data: capacity_free,
                         spanGaps: false,
                     }
                 ]
             }
  
         });
+       this.lineChartTwo.update();
+            
+        });
+
+             
 }
 }
